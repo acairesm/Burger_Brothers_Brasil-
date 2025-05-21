266a616bce3fbfc3639b59c99dc83f88739253e1
Lo_Pedrozo_ <lorenzopedrozo1106@gmail.com>
2025-05-21 12:09:29 -0300

    Versao inicial do BBB

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,11 @@
-  /*import view.MenuPrincipal;
+  /* import view.MenuPrincipal;
 
         public class Main {
             public static void main(String[] args) {
 
                 MenuPrincipal menu = new MenuPrincipal();
 
-                //oi pra todos
-                // oppaaazeazeazeazeaze
-                // vai toma no cu
+
                 menu.exibir();
             }
         }
