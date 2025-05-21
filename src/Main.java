8af1521821b314f60633aad1a73ca9c448e28faf
aslamrekik <aslamrekik.br@gmail.com>
2025-05-21 15:52:58 +0100

    A
    Merge branch 'main' of https://github.com/acairesm/Burger_Brothers_Brasil-
    
    bla bla bla bla
    uga uga uga
    git.

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ import view.MenuPrincipal;
 
                         //oi lore
                         // oi andre g
+                        // oiiiiiiiiiiii
                 menu.exibir();
             }
         }
