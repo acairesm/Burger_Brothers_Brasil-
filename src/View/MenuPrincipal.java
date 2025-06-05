2521a8f895d6b78a0953c64b1c457175e4016d48
André Caires <acairesm4777@gmail.com>
2025-06-05 11:53:18 -0300

    arrume o bug do menurprincipal de voltar

--- src/View/MenuPrincipal.java
+++ src/View/MenuPrincipal.java
@@ -19,22 +19,17 @@ public class MenuPrincipal {
             System.out.println(ANSI_GREEN + "📋 [1] Gerenciar Clientes" + ANSI_RESET);
             System.out.println(ANSI_GREEN + "🛒 [2] Gerenciar Produtos" + ANSI_RESET);
             System.out.println(ANSI_GREEN + "📦 [3] Gerenciar Pedidos" + ANSI_RESET);
+
             System.out.println(ANSI_RED + "🚪 [0] Sair" + ANSI_RESET);
             System.out.println(ANSI_BLUE + "==========================" + ANSI_RESET);
             opcao = InputHelper.lerInt("Digite uma opção :");
 
             switch (opcao) {
-                case 1:
-                    MenuCliente.exibir();
-                case 2:
-                    MenuProduto.exibir();
-                case 3:
-                    MenuPedido.exibir();
-                case 0:
-                    System.out.println(ANSI_GREEN + "Saindo do sistema. Até logo!" + ANSI_RESET);
-                    break;
-                default:
-                    System.out.println(ANSI_RED + "Opção inválida! Tente novamente." + ANSI_RESET);
+                case 1 -> MenuCliente.exibir();
+                case 2 -> MenuProduto.exibir();
+                case 3 -> MenuPedido.exibir();
+                case 0 -> System.out.println(ANSI_GREEN + "Saindo do sistema. Até logo!" + ANSI_RESET);
+                default -> System.out.println(ANSI_RED + "Opção inválida! Tente novamente." + ANSI_RESET);
             }
         } while (opcao != 0);
     }
